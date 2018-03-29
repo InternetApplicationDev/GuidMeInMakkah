9d29c5e5c695e3fcf13f2c9806a9bfa74a8bc8ab
Ebtsam <ebtisamalkhozai@outlook.com>
2018-03-29 10:36:19 +0300

    add my file

---
+++ Project.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+ <link rel="stylesheet" type="text/css" href="project.css">
+</head>
+<body>
+
+
+<div class ="header">
+
+PROJECT 
+<div class="logo"> <img class="img" src="restaurant.png" alt="LOGO" > </div>
+
+ </div>
+ <hr>
+
+	<h1> Makkah: </h1>
+	<form action="pro2.php" method="POST">
+<div class="container"> 
+
+	<?php
+
+$count = 15;
+for ($i = 0; $i < $count; $i++) {
+
+	echo " 	 <div class=\"box\">
+  <img src=\"java.png\" alt=\"Avatar\" class=\"img\">
+  <div class=\"overlay\">
+   <a href=\"pro2.php\"  type=\"button\" value=\"Input Button\" class=\"text\"> Click </a>
+  </div>
+</div>";}
+?>
+<div class="footer">Footer</div>
+</div>
+</form>
+</body>
+</html>
+
