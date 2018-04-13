a2ca3bfe23df9c2a751df8418e3e54d96d287857
IsraaSamkari <israasamkari@hotmail.com>
2018-04-13 15:13:28 +0300

    edit indevisual profile page
    edit and add top conteiner

--- individualprofile.php
+++ individualprofile.php
@@ -1,11 +1,15 @@
 <!DOCTYPE html>
 <html class="theBackGround">
 <head>
-  <link rel="stylesheet" type="text/css" href="CSS/style.css">
-  <script type = "text/javascript" src = "JS/javaScript.js"></script>
 
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
+  
+  <script type = "text/javascript" src = "JS/javaScript.js"></script>
+  <script type = "text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+  <script type = "text/javascript" src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.8.18/jquery-ui.min.js"></script>
+  
 </head>
-<body class="centerPage">
+<body class="centerPage" onload="Slider()">
 
   <div class="navbar">
     <ul class="navmenu">
@@ -23,7 +27,33 @@
   </div>
 
   <hr>
-
+ </br>
+  <div class="fix_layout_center">
+	  <div class="emptyBlackBackground">
+			
+	  </div>
+	  <div class="imageIntro">
+		<img src="images/cafe-intro-backfound.jpg"  alt="image"/>
+	  </div>
+	  <div class="textBlackBackground">
+		<p id="textSliderImg">Wellcome To </br> ____ </br> Coffee shop name</p>
+	  </div>
+	  <img class="starsBlackBackground" id="star_0" src="images/stars rate/2.png" alt="image"/>
+	 
+  </div>
+  
+  </br>
+  <p class="pathThisPageAsText"> home  > list > coffee shope name </p>
+  <table class="tableButtonReservation">
+	<tr>
+	<th>&nbsp &nbsp  <font color="white">150</font> <font color="yellow">RS</font></th>
+	<th> <button class="buttonReservation">Reservation</button> </th>
+	</tr>
+  </table>
+  
+  </br>
+  </br>
+  
 
 <div class="footer">Footer</div>
 
