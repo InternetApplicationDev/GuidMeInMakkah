b7af50a631a5a2295f9828e60ce95cbddd0a65b4
Joanna-1996 <Joanna.a.s.1996@Gmail.com>
2018-04-18 17:07:45 +0300

    Merge mine and esraa code ... profile

--- profile.php
+++ profile.php
@@ -19,27 +19,53 @@ while($row = mysqli_fetch_array($userInformation)){
   $userTwitter = $row['user_twitter'];
   $userFacebook = $row['user_facebook'];
 }
-<<<<<<< HEAD
 // delete account
 if (isset($_GET['Delete'])) {
   echo '
-=======
-?>
-
-<?php
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://unpkg.com/sweetalert2@7.17.0/dist/sweetalert2.all.js"></script>
+  <script>
+  $( document ).ready(function() {
+    swal({
+      title: \'Are you sure?\',
+      text: "You won\'t be able to revert this!",
+      type: \'warning\',
+      showCancelButton: true,
+      confirmButtonColor: \'#3085d6\',
+      cancelButtonColor: \'#d33\',
+      confirmButtonText: \'Yes, delete it!\'
+    }).then((result) => {
+      if (result.value) {';
+        $deleteAccount = mysqli_query($dbc,"DELETE FROM user where user_id = $theuser LIMIT 1");
+        setcookie("theuser","",time()-60*60*24*30,'/');
+        echo 'swal({
+          title: \'Your account has been deleted\',
+          type: \'success\',
+          showConfirmButton: false,
+          timer: 1500,
+        }).then(function(){
+          window.location.href = \'index.php\';
+        });
+      }})
+    });
+    </script>';
+  }
+  mysqli_close($dbc);
+  ?>
+  <?php
 function searchUserNameInIndexFile($userName){
 	$data = file ('Users/Files/filesCommentIndex.txt');
 	$fp = fopen('Users/Files/filesCommentIndex.txt', "r");
 
 	$n = count ($data);
-	$result = 0; 
+	$result = 0;
 	$tmpFilePointer=0; // depend on this value it will chane seek pointer
 	$tmpName=""; // find match between user name and useres names in file
 	$tmpPage=""; // find match between page name and pages names in file
 	$tmpLengthOfFoundName; // to calculat the position of page names
-	
+
 	$pagenameAsArr=array();
-	
+
 	// search user name from file, if it found change var result to 1
 	for ($i = 0; $i < $n; $i++) { // search line by line
 		for ($coun = 1; $coun < strlen($data[$i]); $coun++){ // search char by char in the current line
@@ -52,8 +78,8 @@ function searchUserNameInIndexFile($userName){
 				$tmpName .=$tmpChar; // to make full user name from char pices
 
 		}
-		
-		
+
+
 		if($tmpName == $userName){ // found user name from file
 			$tmpFilePointer+=$tmpLengthOfFoundName; // calculat the postion of page name for this user
 			$result=1; // found user name
@@ -64,7 +90,7 @@ function searchUserNameInIndexFile($userName){
 		$tmpName=""; // to make another user name from file
 	}
 
-	
+
 	// if found user name than go search for page name
 	 //else return result value 0 to add user name and page name to file
 	if ($result==1){
@@ -115,7 +141,7 @@ function retrieveUsersComments($userNameWithHisComment,$user_name){
 		else
 			$userName .=$tmpChar; // to make full user name from char pices
 	}
-	
+
 	if($user_name != $userName){
 		return array("","");
 	}
@@ -138,7 +164,7 @@ function retrieveAllComment($userName,$pageName){
 	$path ="Users/Files/Comments/".$pageName.".txt";
 	$data = file ($path);
 	$n = count ($data);
-	
+
 	$tempUserName;
 	for ( $i=0; $i < $n; $i++){
 		$getUseANDCom = retrieveUsersComments($data[$i],$userName);
@@ -146,56 +172,11 @@ function retrieveAllComment($userName,$pageName){
 			array_push($arrOfCommInThisPage,$getUseANDCom[1]);
 		}
 	}
-	
+
 	return $arrOfCommInThisPage;
 }
 
 ?>
-
-<!DOCTYPE html>
-<html class="theBackGround">
-<head>
-  <title>Profile</title>
-  <!-- Font -->
-  <link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
-  <!-- customized CSS and JS -->
-  <link rel="stylesheet" type="text/css" href="CSS/style.css">
-  <script type="text/javascript">var userEmail = "<?= $userEmail ?>";</script>
-  <script type = "text/javascript" src = "JS/javaScript.js"></script>
-  <!-- Add icon library -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <!-- Sweet Alert  -->
->>>>>>> a6dc5e6475459282148099a953be9874ecce1324
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://unpkg.com/sweetalert2@7.17.0/dist/sweetalert2.all.js"></script>
-  <script>
-  $( document ).ready(function() {
-    swal({
-      title: \'Are you sure?\',
-      text: "You won\'t be able to revert this!",
-      type: \'warning\',
-      showCancelButton: true,
-      confirmButtonColor: \'#3085d6\',
-      cancelButtonColor: \'#d33\',
-      confirmButtonText: \'Yes, delete it!\'
-    }).then((result) => {
-      if (result.value) {';
-        $deleteAccount = mysqli_query($dbc,"DELETE FROM user where user_id = $theuser LIMIT 1");
-        setcookie("theuser","",time()-60*60*24*30,'/');
-        echo 'swal({
-          title: \'Your account has been deleted\',
-          type: \'success\',
-          showConfirmButton: false,
-          timer: 1500,
-        }).then(function(){
-          window.location.href = \'index.php\';
-        });
-      }})
-    });
-    </script>';
-  }
-  mysqli_close($dbc);
-  ?>
   <!DOCTYPE html>
   <html class="theBackGround">
   <head>
@@ -286,11 +267,11 @@ function retrieveAllComment($userName,$pageName){
         <!-- <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" width="512px" height="512px" viewBox="0 0 548.157 548.157" style="enable-background:new 0 0 548.157 548.157;" xml:space="preserve"><g transform="matrix(0.0676446, 0, 0, 0.0676446, 255.539, 255.539)"><link xmlns="" type="text/css" id="dark-mode" rel="stylesheet" class="active-path"/><style xmlns="" type="text/css" id="dark-mode-custom-style" class="active-path"/><g>
         <path d="M545.027,112.765c-3.046-6.471-7.57-11.657-13.565-15.555c-5.996-3.9-12.614-5.852-19.846-5.852H292.351   c-11.04,0-20.175,4.184-27.408,12.56L9.13,396.279c-4.758,5.328-7.661,11.56-8.708,18.698c-1.049,7.139-0.144,13.941,2.712,20.417   c3.044,6.468,7.564,11.652,13.561,15.553c5.997,3.898,12.612,5.853,19.845,5.853h219.268c11.042,0,20.177-4.179,27.41-12.56   l255.813-292.363c4.75-5.33,7.655-11.561,8.699-18.699C548.788,126.039,547.877,119.238,545.027,112.765z M255.811,420.254H36.54   l95.93-109.632h219.27L255.811,420.254z" data-original="#000000" class="active-path" fill="#000000"/>
       </g></g> </svg> -->
-      
+
 	 <div class="content">
 
 		 <h1>Comment</h1>
-     
+
 		<div class="containerComments" >
 		<?php
 			$getCommentPage = searchUserNameInIndexFile($userName);
@@ -298,7 +279,7 @@ function retrieveAllComment($userName,$pageName){
 				for ( $pages=0; $pages < sizeof($getCommentPage); $pages++){
 					$comments = retrieveAllComment($userName,$getCommentPage[$pages]);
 					for ( $i=0; $i < sizeof($comments); $i++){
-					?>	
+					?>
 						<div class="commentsUserBox" >
 							<p class="userNameInComment"><?php echo $userName; ?></p>
 							<img src="images/pesonal icon.jpg" />
@@ -312,13 +293,13 @@ function retrieveAllComment($userName,$pageName){
 				echo "you do not have any comment <br> <br>";
 			}
 			?>
-			
-			
+
+
 
 		</div>
 
 	</div>
-	
+
 	</div>
   </div>
   <div class="footer" id="theFooter">
