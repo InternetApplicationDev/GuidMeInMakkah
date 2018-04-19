5d4853bf1f0dab045041fc2454a1f5efaa6f4210
Ebtsam Alkhuzai <ebtsamalkhuzai@gmail.com>
2018-04-19 15:34:12 +0300

    login and sign up

--- Registration.php
+++ Registration.php
@@ -9,12 +9,12 @@ if (isset($_POST['firstName'])) {
   $lastName = $_POST['lastname'];
   $email = $_POST['Email'];
   $pass = $_POST['password'];
-  $dbc = mysqli_connect("localhost","root","root");
+$dbc = mysqli_connect("localhost","root","root");
+  //$dbc = mysqli_connect("localhost","root","12345678");
   mysqli_select_db($dbc,"db");
   $insertNewUser = "INSERT INTO user (user_email,user_password,user_first_name,user_last_name)
   VALUES ('$email','$pass','$firstName','$lastName')";
   @mysqli_query ($dbc, $insertNewUser);
-  // the select is to get the user id, to store it in the cookie
   if (mysqli_num_rows(mysqli_query($dbc,"SELECT * From user where user_email = '$email' AND user_password ='$pass'"))) {
     $result = mysqli_query($dbc,"SELECT * From user where user_email = '$email' AND user_password ='$pass'");
     while ($row = mysqli_fetch_array($result)) {
@@ -57,11 +57,11 @@ mysqli_close($dbc);
 <!DOCTYPE html>
 <html class="theBackGround">
 <head>
-  <title>Sign Up</title>
+  <title>RegistrationForm</title>
   <!-- Font -->
+  <link rel="stylesheet" type="text/css" href="CSS/style.css">
   <link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
   <!-- customized CSS and JS -->
-  <link rel="stylesheet" type="text/css" href="CSS/style.css">
   <script type="text/javascript" src= "JS/javaScript.js"></script>
   <!-- Sweet Alert  -->
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
@@ -69,10 +69,14 @@ mysqli_close($dbc);
   <!-- pace preloader -->
   <script src="JS/pace.js"></script>
 </head>
-<body class="centerPage">
-  <div class="navbar">
+
+<body class="content">
+
+  <!--
+<div class="navbar">
+
     <ul class="navmenu">
-      <li><a href="index.php"><img src="images/logo-small.png"/></a></li>
+      <li><a href ="index.php"><img src="images/logo-small.png"/></a></li>
       <li><a href="listPage.php?id=1">Cafe</a></li>
       <li><a href="listPage.php?id=2">Restaurants</a></li>
       <li><a href="listPage.php?id=3">Cafe & Restaurants</a></li>
@@ -84,63 +88,32 @@ mysqli_close($dbc);
         <li class="navmenu-right"><a href="Registration.php">Sign Up</a></li>
       <?php } ?>
     </ul>
-  </div>
+  </div> -->   
 
-  <div class ="header">
-    Sign Up
-    <div class="logo"> <img class="img" src="images/icons/tray_black.png" alt="LOGO" > </div>
-  </div>
-  <hr>
+ <a class ="signLogo" href ="index.php"><img src="images/logo-small-black.png"/></a>
+  <h2 class = "welcomeText"> Welcome To Guide Me In Makkah </h2>
+   <h3 color="gray" class = "welcomeText"> creat new account </h3>
+
+
   <form method="post" onSubmit="return validateForm();">
-    <div class="container">
-      <div>
-        <div>
-          <!-- name -->
-          <div class= "register1">First Name : <span style="color: red;"> *</span><br/>
-            <input type="text" id="Name" name="firstName" class="roundTextArea"/>
-          </div>
-          <br/>
-          <!-- last name  -->
-          <div class= "register1">Last Name : <span style="color: red;"> *</span> <br/>
-            <input type="text" id="l_name" name="lastname" class="roundTextArea"/>
-          </div>
-          <br/>
-          <div class= "register1">E-mail : <span style="color: red;"> *</span><br/>
-            <input type="text" id="Email" name="Email"  placeholder="example@xxxxx.com" class="roundTextArea"/>
-          </div>
-          <br/>
-          <div class= "register1"> Password :<span style="color: red;"> *</span><br/>
-            <input type="password" id="password" name="password"class="roundTextArea"/>
-          </div>
-          <br/>
-          <div class= "register1"> Confirm Password :<span style="color: red;"> *</span> <br/>
-            <input type="password" id="cpassword" name="cpassword" class="roundTextArea"/>
-          </div>
-          <br/>
-          <div class= "regesterSubmit"><input name="Submit" value="Submit" type="submit" class="submitbutton"/>
-          </div>
-        </div>
-      </div>
-    </form>
-    <div class="footer" id="theFooter">
-      <div class="footbar">
-        <ul class="footmenu">
-          <li><a href="#" onclick="showDevFunction()">Developers</a></li>
-          <li><a href="about.php">About us</a></li>
-          <li><a href="contactUs.php">Contact us</a></li>
-          <li><a href="#" onclick="showSiteMap()">siteMap</a></li>
-        </ul>
-        <div id="developersNames">
-          <ul class="devNames">
-            <li>Ebtsam Alkhuzai</li>
-            <li>Esraa Samkari</li>
-            <li>Joanna Assaeedi</li>
-            <li>Joud Alajlan</li>
-            <li>Rozan Alghamdi</li>
-            <li>Wafaa Alshaikhi</li>
-          </ul>
-        </div>
-      </div>
-    </div>
+      
+            <input  type="text" id="Name" name="firstName" placeholder="First Name"  />
+   
+            <input  type="text" id="l_name" name="lastname" placeholder="Last Name" />
+
+            <input type="text" id="Email" name="Email"  placeholder="example@xxxxx.com" />
+  
+     
+            <input  type="password" id="password" name="password" placeholder="password"  />
+
+            <input type="password" id="cpassword" name="cpassword" placeholder="Confirm password" />
+
+           <input name="Submit" value="Submit" type="submit"/>
+       
+
+          <h3 class = "welcomeText"> already have account <a href="Login.php">sign in </a> </h3>
+
+</form>
+
   </body>
   </html>
